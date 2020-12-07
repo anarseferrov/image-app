ff238730643d6a95427e59eca33529fd8464eabe
Anar Seferov <1anarseferov@gmail.com>
2020-12-07 18:54:02 +0400

    Updates

---
+++ precache-manifest.b45f5d6b2f945018bb087c3c60e2e05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05beb6eab06690b990346d79bc665b42",
+    "url": "/image-app/index.html"
+  },
+  {
+    "revision": "cf5d823fcbad8fc3e225",
+    "url": "/image-app/static/css/main.a0343ec7.chunk.css"
+  },
+  {
+    "revision": "2df65c9ba5b16ab97620",
+    "url": "/image-app/static/js/2.86e57007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-app/static/js/2.86e57007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5d823fcbad8fc3e225",
+    "url": "/image-app/static/js/main.4e1ad9cc.chunk.js"
+  },
+  {
+    "revision": "afaf53f73e7a896649bc",
+    "url": "/image-app/static/js/runtime-main.a73d7d50.js"
+  }
+]);
